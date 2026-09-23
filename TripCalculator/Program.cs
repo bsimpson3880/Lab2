5db1d67a7c82953e926a57549e9b19d2d38f1e6f
Bryce Simpson <brycesimpson440@gmail.com>
2026-09-23 13:33:25 -0400

    first commit

---
+++ TripCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("What was the round trip in miles?");
+int milesForRoundTrip = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("What is the miles per gallon of the vehicle?");
+int milesPerGallon = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("What is the price per gallon of gas?");
+double pricePerGallon = Convert.ToDouble(Console.ReadLine());
+
+//do the math
+
+double gallonsNeeded = milesForRoundTrip / (double)milesPerGallon;
+
+double fuelCost = gallonsNeeded * pricePerGallon;
+
+// do the output.
+System.Console.WriteLine("Gallons needed: " + gallonsNeeded.ToString("F2"));
+System.Console.WriteLine("Fuel Cost: " + fuelCost.ToString("C"));
+
